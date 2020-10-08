d881bd4d1e2a9b7eb44ad25221d4ef457ffe38e3
gorstak17 <72560271+gorstak17@users.noreply.github.com>
2020-10-08 15:32:07 +0200

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stranica</title>
+    <h1>WSIT</h1>
+</head>
+<body>
+    <?php
+    echo "<br> Loncar <br> Aleksandar <br> S73/19";
+
+    ?>
+</body>
+</html>
